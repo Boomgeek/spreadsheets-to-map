277bc2e3c899af0d25f63fc3d4b06f1c92155516
Supanut Dokmaithong <supanut.geek@gmail.com>
2016-03-25 10:13:48 +0700

    create function callSpreadSheet for call SpreadSheet with json to obj

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 var map;
+var spreadSheetObj;
+
 $(function(){
 
 	init();
+	callSpreadSheet("1rVIcAH6_FaAHXr0JSxsXuEJLCBsTt7PTWqPmjlueOws");
 
 	function init() {
 		map = new longdo.Map({
 			placeholder: document.getElementById('map')
+		});
+	}
+
+	function callSpreadSheet(sheetId){
+		$.ajax({
+			url: "https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec",
+			type: "GET",
+			datatype: "jsonp",
+			data: "id="+sheetId+"&sheet=Sheet1",
+			success: function(res){
+				console.log(res);
+				spreadSheetObj = res;
+			} 
 		});
 	}
 
