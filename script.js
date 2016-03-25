cd16a268916f83573f65f5c0af68e4f6bc83556b
Supanut Dokmaithong <supanut.geek@gmail.com>
2016-03-25 10:37:28 +0700

    success load spread sheet data to json and add marker to map

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var spreadSheetObj;
 $(function(){
 
 	init();
-	callSpreadSheet("1rVIcAH6_FaAHXr0JSxsXuEJLCBsTt7PTWqPmjlueOws");
+	callSpreadSheet("1rVIcAH6_FaAHXr0JSxsXuEJLCBsTt7PTWqPmjlueOws", function(){
+		jsonToMarker();
+	});
 
 	function init() {
 		map = new longdo.Map({
@@ -12,7 +14,7 @@ $(function(){
 		});
 	}
 
-	function callSpreadSheet(sheetId){
+	function callSpreadSheet(sheetId,callback){
 		$.ajax({
 			url: "https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec",
 			type: "GET",
@@ -21,8 +23,36 @@ $(function(){
 			success: function(res){
 				console.log(res);
 				spreadSheetObj = res;
+				callback();
 			} 
 		});
+	}
+
+	function jsonToMarker(){
+		for(i=0; i<spreadSheetObj['Sheet1'].length; i++){
+			icon = spreadSheetObj['Sheet1'][i]['Icon'];
+			title = spreadSheetObj['Sheet1'][i]['Title'];
+			description = spreadSheetObj['Sheet1'][i]['Description'];
+			zoom = spreadSheetObj['Sheet1'][i]['Zoom'];
+			lat = spreadSheetObj['Sheet1'][i]['Latitude'];
+			lon = spreadSheetObj['Sheet1'][i]['Longitude'];
+			addMarker(icon, title, description, zoom, lat, lon);
+		}
+	}
+
+	function addMarker(icon, title, description, zoom, lat, lon){
+		var marker = new longdo.Marker({ lon: lon, lat: lat },
+		{
+		  title: title,
+		  icon: {
+		    url: icon,
+		    offset: { x: 12, y: 45 }
+		  },
+		  detail: "<b>description:</b>"+description+"<br><b>lat:</b>"+lat+"<br><b>lon:</b>"+lon,
+		  visibleRange: { min: zoom-5, max: zoom },
+		  draggable: false
+		});
+		map.Overlays.add(marker);
 	}
 
 });
